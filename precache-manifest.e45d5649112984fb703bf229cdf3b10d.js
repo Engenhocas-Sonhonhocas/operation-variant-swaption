817984d099b2c16c975692121d3a54e53cf95962
Colin Brayton <gringolalia@gmail.com>
2019-12-28 13:26:49 -0300

    deploy

---
+++ precache-manifest.e45d5649112984fb703bf229cdf3b10d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff5b9e4c3cf0513c1e47",
+    "url": "/various-enigma-solutions/js/about.1fe9424d.js"
+  },
+  {
+    "revision": "f664e774ff370b1cf3a4",
+    "url": "/various-enigma-solutions/css/app.0f19e6c4.css"
+  },
+  {
+    "revision": "f664e774ff370b1cf3a4",
+    "url": "/various-enigma-solutions/js/app.7efc5f94.js"
+  },
+  {
+    "revision": "ccf3fcceb8ebc4d346df",
+    "url": "/various-enigma-solutions/css/chunk-vendors.d4a30ca6.css"
+  },
+  {
+    "revision": "ccf3fcceb8ebc4d346df",
+    "url": "/various-enigma-solutions/js/chunk-vendors.525424c0.js"
+  },
+  {
+    "revision": "3fea563c5316b0039389b59f0a876105",
+    "url": "/various-enigma-solutions/img/hammer-sickle.3fea563c.svg"
+  },
+  {
+    "revision": "73456a1d9ec46ec435c6177021823288",
+    "url": "/various-enigma-solutions/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/various-enigma-solutions/robots.txt"
+  }
+];
